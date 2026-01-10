c43e93e85e60a45ac2700acf6037543f77a3c658
Mxteo2008 <mateo.jimenez.bvm@gmail.com>
2026-01-09 19:38:18 -0500

    Problema 7

--- ejercicios/Problemas/src/paquete07/Problema7.java
+++ ejercicios/Problemas/src/paquete07/Problema7.java
@@ -4,11 +4,51 @@
  * and open the template in the editor.
  */
 package paquete07;
+import java.util.Scanner;
 
 /**
  *
  * @author reroes
  */
 public class Problema7 {
-    
+    static Scanner entrada = new Scanner(System.in);
+    public static void main(String[] args) {
+     
+        String[] listaCiudades = ingresarCiudades();
+
+        imprimirCiudadesCortas(listaCiudades);
+    }
+
+    public static String[] ingresarCiudades() {
+            
+        
+        System.out.println("¿Cuántas ciudades desea ingresar?");
+        int numeroElementos = entrada.nextInt();
+
+        entrada.nextLine(); 
+        
+        String[] arreglo = new String[numeroElementos];
+        
+        for (int i = 0; i < numeroElementos; i++) {
+            System.out.println("Ingrese la ciudad #" + (i + 1) + ":");
+            arreglo[i] = entrada.nextLine();
+        }
+        
+        return arreglo;
+    }
+
+ 
+    public static void imprimirCiudadesCortas(String[] datos) {
+        System.out.println("\nCiudades con 4 o 5 letras");
+        
+        for (int i = 0; i < datos.length; i++) {
+            int longitud = datos[i].length();
+            
+            if (longitud == 4 || longitud == 5) {
+                System.out.printf("%s\n", datos[i]);
+            }
+        }
+    }
 }
+    
+
